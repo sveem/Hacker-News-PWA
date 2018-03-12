e44e006bd7681b365b6b24bc5204ebb0594ca8fa
Svetlin Mladenov <sveem@users.noreply.github.com>
2018-03-12 09:34:00 -0400

    dynamic: Added Material Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,20 @@ export class AppComponent implements OnInit {
   status: String = 'Current Status: Online'
   navigator: Boolean = window.navigator.onLine;
   allNews: any[] = [];
-  
-  constructor(private appService: AppService) {}
+
+  constructor(private appService: AppService) { }
 
   ngOnInit() {
     if (!navigator.onLine) {
-      this.status = 'Current Status: Offline'    
+      this.status = 'Current Status: Offline'
     }
+  console.log('Local Storage', localStorage['newsId']);
+    if (localStorage['newsId']) {
+      console.log('Local Storage on Init', localStorage);
+      this.allNews = JSON.parse(localStorage['newsId']); 
+      console.log('allNews = LocalStorage[newsId]', this.allNews);  
+    }
+    console.log(this.fetchTopNews());
     this.fetchTopNews();
   }
 
@@ -27,8 +34,10 @@ export class AppComponent implements OnInit {
       .subscribe(news => {
         this.allNews = news['articles'];
         console.log('newsIds', this.allNews);
+          localStorage['newsId'] = JSON.stringify(this.allNews)
+          console.log('Local Storage', localStorage);
       });
-   }
+  }
 }
 
 
@@ -48,7 +57,7 @@ export class AppComponent implements OnInit {
 //   status: String = 'Current Status: Online'
 //   navigator: Boolean = window.navigator.onLine;
 //   allNews;
-  
+
 //   constructor(http: HttpClient) {
 //     let path = `https://newsapi.org/v2/top-headlines?sources=hacker-news&apiKey=6bd90b29cd78473c99ce04b6470c28a3`;  ---- Patern start
 //     this.allNews = http.get<any>(path).pipe(map(obj => obj['articles'])) 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,55 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { AboutComponent } from './about/about.component';
 import { AppService } from './app.service';
+import { RouterModule, Routes } from '@angular/router';
+
+// Angular Material UI
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material';
+import { MatExpansionModule } from '@angular/material/expansion';
+import 'hammerjs';
+
+const appRoutes: Routes = [
+  { path: '', component: NavbarComponent, children: [
+    { path: '', component: HomeComponent },
+    { path: 'news', component: NewsComponent },
+    { path: 'jobs', component: JobsComponent },
+    { path: 'about', component: AboutComponent }]
+  }];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    NewsComponent,
+    JobsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatExpansionModule,
     ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// To-Do: Create separate Ng file
